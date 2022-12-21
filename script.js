3101d430d4e8f5473b17f113e8677e1b3c39d35b
mar <rojanomariano@gmail.com>
2022-12-21 15:59:46 -0800

    created variables for URL's and made a fetch method to get data about a specific city

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var cityName;
+
+// created variable for API key
+var apiKey = "ba17ab7e50334efbdf75c59b609ec00e";
+var baseUrlForOneDay =
+  "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=";
+
 // Reach into the HTMl and grab the search button
 var searchBtn = document.querySelector(".search");
 console.log(searchBtn);
@@ -9,10 +16,10 @@ searchBtn.addEventListener("click", function () {
   // Look at the element we grab to see what properties we can use
   console.dir(searchInputEl);
   // extract the data that we wanted out of the object
-  var cityName = searchInputEl.value;
+  cityName = searchInputEl.value;
   console.log(cityName);
 
-  var url = "https://pokeapi.co/api/v2/pokemon/" + cityName
+  var url = baseUrlForOneDay + cityName;
 
   // use city name to make the http call
   fetch(url)
@@ -21,13 +28,25 @@ searchBtn.addEventListener("click", function () {
       // takes the response data and converts it tot he data you wanted
       return response.json();
     })
-    .then(function (data) {
-      console.log(data); // sanity check
+    .then(function (bacon) {
+      console.log(bacon); // sanity check
 
-      // extract the data we want
-      var imageUrl = data.sprites.front_default;
+      // extract the data we want for today
 
       // put it somewhere on the screen
-      document.querySelector('#pkmn').src = imageUrl
+
+
+      // extract the lat and lon from the data
+var lon = bacon.coord.lon
+var lat = bacon.coord.lat
+      // use it to create the other url
+      var baseUrlForFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+
+      // make a call to the other url
+      // -- then turn the response data into real data
+      // -- then 
+      // -- -- log the data
+      // -- -- extract the info you want from the data
+      // -- -- put it on the screen
     });
 });
